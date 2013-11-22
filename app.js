33298ab4e46053edbe3c4c8e203b5954a1affcfc
Collin Stedman <cstedman@princeton>
2013-11-21 20:42:59 -0500

    Fixed script injection

--- app.js
+++ app.js
@@ -7,8 +7,28 @@ app.use(express.static(__dirname + '/public'));
 
 server.listen(3000);
 
+var intro = "Welcome to Lucidity! Ask questions anonymously and get real-time" +
+            " attention. Please be respectful of those who are trying to learn."
+
+var users = -1;
+var count = 0;
+
 io.sockets.on('connection', function (socket) {
+  users = users+1;
+  setTimeout(function() {
+    socket.emit('update', { content: intro, 
+                            id: count++, 
+                            users: users,
+                            admin: true });
+  }, 500);
   socket.on('query', function (data) {
-    io.sockets.emit('update', { content: data.content });
+    io.sockets.emit('update', { content: data.content, 
+                                id: count++, 
+                                users: users,
+                                admin: false });
+  });
+  socket.on('disconnect', function (data) {
+    users=users-1;
+    io.sockets.emit('users', { users: users });
   });
 });
