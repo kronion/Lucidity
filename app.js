2a58473f8c94c103736696e04459fcd584c02e5d
Collin Stedman <cstedman@princeton>
2014-01-03 03:40:29 -0600

    Cleaned up client-side code, changed rate limit alerts

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var intro = "Welcome to Lucidity! Ask questions anonymously and get real-time" +
             " attention. Please be respectful of those who are trying to learn."
 
 var users = -1;
-var count = 0;
 
 io.sockets.on('connection', function (socket) {
   users = users+1;
@@ -20,7 +19,6 @@ io.sockets.on('connection', function (socket) {
 
   setTimeout(function() {
     socket.emit('update', { content: intro, 
-                            id: count++, 
                             users: users,
                             admin: true });
   }, 500);
@@ -28,7 +26,6 @@ io.sockets.on('connection', function (socket) {
     data.content = data.content.split(/\n/g);
     console.log(data.content);
     io.sockets.emit('update', { content: data.content, 
-                                id: count++, 
                                 users: users,
                                 admin: false });
   });
--- public/script.js
+++ public/script.js
@@ -1,22 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* GLOBAL VARIABLES                                                           */
+/*----------------------------------------------------------------------------*/
+
+/* Socket.io server connection */
 var socket = io.connect('http://localhost:3000');
 
-/* Message part variables */
-var message1 = "<div class='message1' id=";
-var message2 = "<div class='message2' id=";
-var admin = "<div class='admin' id=";
-var middle = "><p>";
-var close = "</p></div>";
-var admin2 = "<div class='admin'></div>";
-var message12 = "<div class='message1'></div>";
-var message22 = "<div class='message2'></div>";
-var paragraph = "<p></p>";
+/* Message variables */
+var admin = "<div class='admin'></div>";
+var message1 = "<div class='message1'></div>";
+var message2 = "<div class='message2'></div>";
+var p = "<p></p>";
+var br = "<br>";
 
 /* User count variables */
 var count1 = "Ask questions with ";
 var count2 = " other users!";
 var countSingle = " other user!";
 
-var bool = true;
+/* Color variable */
+var altColor = true;
 
 /* Character count variables */
 var remaining = " characters remaining.";
@@ -30,14 +32,19 @@ var rateLimit1 = "You are sending messages too quickly. Please wait ";
 var rateLimit2 = "and try again.";
 var second = " second ";
 var seconds = " seconds ";
-var localCount = 0;
+
+/*----------------------------------------------------------------------------*/
+/* FUNCTIONS                                                                  */
+/*----------------------------------------------------------------------------*/
 
 /* Character count animations */
 $('#query').focus(function() {
   if (characters == 199) {
     $('#charcount p').text((limit-characters) + remainingSingle);
   }
-  else $('#charcount p').text((limit-characters) + remaining);
+  else {
+    $('#charcount p').text((limit-characters) + remaining);
+  }
   $('#charcount').slideDown('slow');
 });
 $('#query').focusout(function() {
@@ -50,54 +57,7 @@ var charCount = function(str) {
   return str.length + 29 * newlines;
 };
 
-var send = function(e) {
-  e.preventDefault();
-  characters = charCount($('#query').val());
-  if (characters <= limit) {
-    var notEmpty = $('#query').val().replace(/\n/g, '');
-    if (notEmpty) {
-      if (lastMessage == undefined || lastMessage + 8000 < Date.now()) {
-        socket.emit('query', {
-          content: $('#query').val()
-        });
-        $('#query').val('');
-        characters = 0;
-        $('#charcount p').text((limit-characters) + remaining);
-        lastMessage = Date.now();
-        localCount++;
-      }
-      else {
-        var wait = Math.ceil((lastMessage + 8000 - Date.now())/1000);
-        if ($('#a'+localCount).length == 0) {
-          $('#stream').prepend(admin + "a" + localCount + middle + close); 
-        }
-        if (wait == 1) {
-          $('#a'+localCount+' p').text(rateLimit1 + wait + second
-                                        + rateLimit2);
-        }
-        else {
-          $('#a'+localCount+' p').text(rateLimit1 + wait + seconds 
-                                        + rateLimit2);
-        }
-        $('#a'+localCount).slideDown('slow');
-      }
-    }
-  }
-};
-
-$('#send').on('click', function(e) {
-  send(e);
-});
-
-$('#query').keydown(function(e) {
-  if (e.which == 13 && !e.shiftKey) {
-    send(e);
-  }
-  else {
-    charWarning();
-  }
-});
-
+/* Character limit warning style */
 function charWarning() {
   setTimeout(function() {
     characters = charCount($('#query').val());
@@ -116,6 +76,69 @@ function charWarning() {
   }, 50);
 }
 
+/* Display the message with the proper format */
+function displayMessage(message, content) {
+  if (content.length == 1) {
+    $(p).prependTo($(message).prependTo('#stream')).text(content[0]);
+  }
+  else {
+    $('#stream').prepend(message);
+    for (var i=0; i < content.length; i++) {
+      if (content[i]) {
+        $(p).appendTo('#stream div:first').text(content[i]);
+      }
+      else {
+        $('#stream div').first().append(br);
+      }
+    }
+  }
+  $('#stream div').first().slideDown('slow');
+}
+
+/* Send a message to the socket.io server */
+var send = function(e) {
+  e.preventDefault();
+  characters = charCount($('#query').val());
+  if (characters <= limit) {
+    var notEmpty = $('#query').val().replace(/\n/g, '');
+    if (notEmpty) {
+      if (lastMessage == undefined || lastMessage + 8000 < Date.now()) {
+        socket.emit('query', {
+          content: $('#query').val()
+        });
+        $('#query').val('');
+        characters = 0;
+        $('#charcount p').text((limit-characters) + remaining);
+        lastMessage = Date.now();
+      }
+      else {
+        var wait = Math.ceil((lastMessage + 8000 - Date.now())/1000);
+        if (wait == 1) {
+          displayMessage(admin, [rateLimit1 + wait + second + rateLimit2]);
+        }
+        else {
+          displayMessage(admin, [rateLimit1 + wait + seconds + rateLimit2]);
+        }
+      }
+    }
+  }
+};
+
+/* Send button clicked */
+$('#send').on('click', function(e) {
+  send(e);
+});
+
+/* Message sent by 'Enter' key */
+$('#query').keydown(function(e) {
+  if (e.which == 13 && !e.shiftKey) {
+    send(e);
+  }
+  else {
+    charWarning();
+  }
+});
+
 /* Update the user count */
 function userCountUpdate(users) {
   if (users === 1) {
@@ -126,49 +149,30 @@ function userCountUpdate(users) {
   }
 }
 
-/* Display the message with the proper format */
-function displayMessage(message, content) {
-  if (content.length == 1) {
-    $(paragraph).prependTo($(message).prependTo('#stream')).text(content[0]);
-  }
-  else {
-    $('#stream').prepend(message);
-    for (var i=0; i < content.length; i++) {
-      if (content[i]) {
-        $(paragraph).appendTo('#stream div:first').text(content[i]);
-      }
-      else {
-        $('#stream div').first().append('<br>');
-      }
-    }
-  }
-  $('#stream div').first().slideDown('slow');
-}
-
 /* Message has been received */
 socket.on('update', function (data) {
 
   // Admin message
   if (data.admin) {
-    $(paragraph).prependTo($(admin2).prependTo('#stream')).text(data.content);
+    $(p).prependTo($(admin).prependTo('#stream')).text(data.content);
     $('#stream div').first().slideDown('slow');
   }
 
   // Message 1
-  else if (bool) {
-    displayMessage(message12, data.content);
+  else if (altColor) {
+    displayMessage(message1, data.content);
   }
 
   // Message 2
   else {
-    displayMessage(message22, data.content);
+    displayMessage(message2, data.content);
   }
 
   // User count update
   userCountUpdate(data.users);
 
   // Color flip
-  bool = !bool;
+  altColor = !altColor;
 });
 
 /* User update has been received */
