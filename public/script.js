c9efb139569868726cfcbdd4123efb2b4892327d
Collin Stedman <cstedman@princeton>
2014-01-03 01:05:49 -0600

    Removed need for unique message IDs

--- public/script.js
+++ public/script.js
@@ -1,22 +1,30 @@
 var socket = io.connect('http://localhost:3000');
 
+/* Message part variables */
 var message1 = "<div class='message1' id=";
 var message2 = "<div class='message2' id=";
 var admin = "<div class='admin' id=";
 var middle = "><p>";
 var close = "</p></div>";
+var admin2 = "<div class='admin'></div>";
+var message12 = "<div class='message1'></div>";
+var message22 = "<div class='message2'></div>";
+var paragraph = "<p></p>";
 
+/* User count variables */
 var count1 = "Ask questions with ";
 var count2 = " other users!";
 var countSingle = " other user!";
 
 var bool = true;
-      
+
+/* Character count variables */
 var remaining = " characters remaining.";
 var remainingSingle = " character remaining.";
 var limit = 200;
 var characters = 0;
 
+/* Rate limiting variables */
 var lastMessage;
 var rateLimit1 = "You are sending messages too quickly. Please wait ";
 var rateLimit2 = "and try again.";
@@ -24,6 +32,7 @@ var second = " second ";
 var seconds = " seconds ";
 var localCount = 0;
 
+/* Character count animations */
 $('#query').focus(function() {
   if (characters == 199) {
     $('#charcount p').text((limit-characters) + remainingSingle);
@@ -35,6 +44,7 @@ $('#query').focusout(function() {
   $('#charcount').slideUp('slow');
 });
 
+/* Character count function */
 var charCount = function(str) {
   var newlines = str.split('\n').length - 1;
   return str.length + 29 * newlines;
@@ -78,46 +88,17 @@ var send = function(e) {
 $('#send').on('click', function(e) {
   send(e);
 });
+
 $('#query').keydown(function(e) {
   if (e.which == 13 && !e.shiftKey) {
     send(e);
   }
-  else if (e.which == 13 && e.shiftKey) {
-    setTimeout(function() {
-      characters = charCount($('#query').val());
-      if (characters > 200) {
-        $('#charcount p').text((limit-characters) + remaining);
-        $('#charcount').css( { 'background-color': '#996666' });
-      }
-      else if (characters == 199) {
-        $('#charcount p').text((limit-characters) + remainingSingle);
-        $('#charcount').css( { 'background-color': '#646464' });
-      }
-      else {
-        $('#charcount p').text((limit-characters) + remaining);
-        $('#charcount').css( { 'background-color': '#646464' });
-      }
-    }, 50);
-  }
   else {
-    setTimeout(function() {
-      characters = charCount($('#query').val());
-      if (characters > 200) {
-        $('#charcount p').text((limit-characters) + remaining);
-        $('#charcount').css( { 'background-color': '#996666' });
-      }
-      else if (characters == 199) {
-        $('#charcount p').text((limit-characters) + remainingSingle);
-        $('#charcount').css( { 'background-color': '#646464' });
-      }
-      else {
-        $('#charcount p').text((limit-characters) + remaining);
-        $('#charcount').css( { 'background-color': '#646464' });
-      }
-    }, 50);
+    charWarning();
   }
 });
-function paste(e) {
+
+function charWarning() {
   setTimeout(function() {
     characters = charCount($('#query').val());
     if (characters > 200) {
@@ -135,80 +116,62 @@ function paste(e) {
   }, 50);
 }
 
+/* Update the user count */
+function userCountUpdate(users) {
+  if (users === 1) {
+    $('#count').text(count1 + users + countSingle);
+  }
+  else {
+    $('#count').text(count1 + users + count2);
+  }
+}
+
+/* Display the message with the proper format */
+function displayMessage(message, content) {
+  if (content.length == 1) {
+    $(paragraph).prependTo($(message).prependTo('#stream')).text(content[0]);
+  }
+  else {
+    $('#stream').prepend(message);
+    for (var i=0; i < content.length; i++) {
+      if (content[i]) {
+        $(paragraph).appendTo('#stream div:first').text(content[i]);
+      }
+      else {
+        $('#stream div').first().append('<br>');
+      }
+    }
+  }
+  $('#stream div').first().slideDown('slow');
+}
+
+/* Message has been received */
 socket.on('update', function (data) {
+
+  // Admin message
   if (data.admin) {
-    $('#stream').prepend(admin + data.id + middle + close);
-    $('#'+data.id+' p').text(data.content);
-    $('#'+data.id).slideDown('slow');
-    if (data.users === 1) {
-      $('#count').text(count1 + data.users + countSingle);
-    }
-    else {
-      $('#count').text(count1 + data.users + count2);
-    }
+    $(paragraph).prependTo($(admin2).prependTo('#stream')).text(data.content);
+    $('#stream div').first().slideDown('slow');
   }
+
+  // Message 1
   else if (bool) {
-    $('#stream').prepend(message1 + data.id + middle + close);
-    if (data.content.length == 1) {
-      $('#'+data.id+' p').text(data.content[0]);
-      $('#'+data.id).slideDown('slow');
-    }
-    else {
-      for (var i=0; i < data.content.length-1; i++) {
-        if (data.content[i]) {
-          $('#'+data.id+' p:last').text(data.content[i]);
-        }
-        else $('#'+data.id).append('<br>');
-        $('#'+data.id).append('<p></p>');
-      }
-      if (data.content[data.content.length-1]) {
-        $('#'+data.id+' p:last').text(data.content[data.content.length-1]);
-      }
-      else $('#'+data.id).append('<br>');
-      $('#'+data.id).slideDown('slow');
-    }
-    if (data.users === 1) {
-      $('#count').text(count1 + data.users + countSingle);
-    }
-    else {
-      $('#count').text(count1 + data.users + count2);
-    }
-    bool = false;
+    displayMessage(message12, data.content);
   }
+
+  // Message 2
   else {
-    $('#stream').prepend(message2 + data.id + middle + close);
-    if (data.content.length == 1) {
-      $('#'+data.id+' p').text(data.content[0]);
-      $('#'+data.id).slideDown('slow');
-    }
-    else {
-      for (var i=0; i < data.content.length-1; i++) {
-        if (data.content[i]) {
-          $('#'+data.id+' p:last').text(data.content[i]);
-        }
-        else $('#'+data.id).append('<br>');
-        $('#'+data.id).append('<p></p>');
-      }
-      if (data.content[data.content.length-1]) {
-        $('#'+data.id+' p:last').text(data.content[data.content.length-1]);
-      }
-      else $('#'+data.id).append('<br>');
-      $('#'+data.id).slideDown('slow');
-    }
-    if (data.users === 1) {
-      $('#count').text(count1 + data.users + countSingle);
-    }
-    else {
-      $('#count').text(count1 + data.users + count2);
-    }
-    bool = true;
+    displayMessage(message22, data.content);
   }
+
+  // User count update
+  userCountUpdate(data.users);
+
+  // Color flip
+  bool = !bool;
 });
+
+/* User update has been received */
 socket.on('users', function (data) {
-  if (data.users === 1) {
-    $('#count').text(count1 + data.users + countSingle);
-  }
-  else {
-    $('#count').text(count1 + data.users + count2);
-  }
+  userCountUpdate(data.users);
 });
